05af15071905b9cbe80bd89b8c084fb6b85a9736
LightPride <32414745+LightPride@users.noreply.github.com>
2024-02-15 19:16:21 +0000

    Deploying to gh-pages from @ LightPride/DrinkMaster-FrontEnd@78caf818967e912c051ec711fbf41e7b9bafb2e5 🚀

---
+++ assets/WelcomePage-c626a2be.js
@@ -0,0 +1,20 @@
+import{n as t,j as e}from"./index-c88b7c1b.js";const a=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 500px;
+`,n=t.h1`
+  margin: 0;
+  margin-right: 10px;
+  color: #ffffff;
+  text-shadow:
+    0 0 5px #00baff,
+    0 0 10px #00baff,
+    0 0 20px #00baff,
+    0 0 40px #00baff,
+    0 0 80px #00baff;
+`,f=t.img`
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+`,s="/DrinkMaster-FrontEnd/assets/example-def0f7cb.png",o=()=>e.jsxs(a,{children:[e.jsx(n,{children:"Welcome Page"}),e.jsx(f,{src:s,alt:"Example"})]});export{o as default};
