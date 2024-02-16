6aaa92d9ffb09e59cb18fcb012e5194187bf7c3c
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-16 16:23:04 +0200

    add RecipePreparation.jsx

--- src/components/DrinkPageComponents/RecipePreparation/RecipePreparation.jsx
+++ src/components/DrinkPageComponents/RecipePreparation/RecipePreparation.jsx
@@ -1,10 +1,9 @@
-import recipeDefaultMobile from '../../../images/Cocktails';
-// import recipeDefaultMobile from '../../images/recipeDefaultPhoto/recipe-default-mobile.jpg';
-import recipeDefaultMobile2x from '../../images/recipeDefaultPhoto/recipe-default-mobile@2x.jpg';
-import recipeDefaultTablet from '../../images/recipeDefaultPhoto/recipe-default-tablet.jpg';
-import recipeDefaultTablet2x from '../../images/recipeDefaultPhoto/recipe-default-tablet@2x.jpg';
-import recipeDefaultDesktop from '../../images/recipeDefaultPhoto/recipe-default-desktop.jpg';
-import recipeDefaultDesktop2x from '../../images/recipeDefaultPhoto/recipe-default-desktop@2x.jpg';
+import cocktailDefaultMobile from '../../../images/Cocktails/coctails@1xMob.jpg';
+import cocktailDefaultMobile2x from '../../../images/Cocktails/coctails@2xMob.jpg';
+import cocktailDefaultTablet from '../../../images/Cocktails/coctails@1xTab.jpg';
+import cocktailDefaultTablet2x from '../../../images/Cocktails/coctails@2xTab.jpg';
+import cocktailDefaultDesktop from '../../../images/Cocktails/coctails@1xDesk.jpg';
+import cocktailDefaultDesktop2x from '../../../images/Cocktails/coctails@2xDesk.jpg';
 
 import {
   RecipePreparationTitle,
@@ -25,18 +24,18 @@ export const RecipePreparation = ({ instructions }) => {
         <picture>
           <source
             media="(min-width: 1440px)"
-            srcSet={`${recipeDefaultDesktop} 1x, ${recipeDefaultDesktop2x} 2x`}
+            srcSet={`${cocktailDefaultDesktop} 1x, ${cocktailDefaultDesktop2x} 2x`}
           />
           <source
             media="(min-width: 768px)"
-            srcSet={`${recipeDefaultTablet} 1x, ${recipeDefaultTablet2x} 2x`}
+            srcSet={`${cocktailDefaultTablet} 1x, ${cocktailDefaultTablet2x} 2x`}
           />
           <source
             media="(min-width: 375px)"
-            srcSet={`${recipeDefaultMobile} 1x, ${recipeDefaultMobile2x} 2x`}
+            srcSet={`${cocktailDefaultMobile} 1x, ${cocktailDefaultMobile2x} 2x`}
           />
           <RecipePreparationImage
-            src={recipeDefaultMobile}
+            src={cocktailDefaultMobile}
             alt="drinks photo"
           />
         </picture>
