3974f10aa30c3a3680876c121cedba51a2f44349
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-18 01:48:48 +0200

    DrinkPage work version (from recipe json)

--- src/components/DrinkIngredientList/DrinkIngredientsList.jsx
+++ src/components/DrinkIngredientList/DrinkIngredientsList.jsx
@@ -4,16 +4,13 @@ import {
   IngredientsList,
 } from './DrinkIngredientsList.styled';
 
-// import drink from '../../../data/drink.json';
-// import ingredients from '../../../data/ingredients.json';
-
 const DrinkIngredientsList = ({ ingredients }) => {
   return (
     <>
       <IngredientsTitle>Ingredients</IngredientsTitle>
       <IngredientsList>
         {ingredients.map((ingredient) => (
-          <li key={ingredient.id}>
+          <li key={ingredient._id}>
             <DrinkIngredientItem
               title={ingredient.title}
               measure={ingredient.measure}
--- src/pages/DrinkPage/DrinkPage.jsx
+++ src/pages/DrinkPage/DrinkPage.jsx
@@ -2,25 +2,15 @@ import { Container, DrinkPageWrapper } from './DrinkPage.styled';
 import DrinkPageHero from '../../components/DrinkPageHero/DrinkPageHero';
 import DrinkIngredientsList from '../../components/DrinkIngredientList/DrinkIngredientsList';
 import RecipePreparation from '../../components/RecipePreparation/RecipePreparation';
-// import { Container } from '../../components/Container/Container.styled';
 
-// import drink from '../../data/drink.json';
 import recipes from '../../helpers/data/recipes.json';
-// import ingredients from '../../data/ingredients.json';
-// import ingredients from '../../helpers/data/recipes.json';
 
 const DrinkPage = () => {
-  // console.log(drink[0].drink);
-  // console.log(drink[0].glass);
-  // console.log(drink[0].alcoholic);
-  // console.log(drink[0].description);
-  // console.log(drink[0].drinkThumb);
-  // console.log(ingredients[0].title);
   return (
     <DrinkPageWrapper>
       <Container>
         <DrinkPageHero
-          // id={drink._id}
+          id={recipes._id}
           name={recipes[0].drink}
           glass={recipes[0].glass}
           alcoholic={recipes[0].alcoholic}
