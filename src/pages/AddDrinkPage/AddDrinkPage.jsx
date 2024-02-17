8fa74186e3aad82806ebc68f94e3a5f387c0e381
sidlicki <130383232+sidlicki@users.noreply.github.com>
2024-02-17 13:18:58 +0100

    Update AddDrinkPage.jsx

--- src/pages/AddDrinkPage/AddDrinkPage.jsx
+++ src/pages/AddDrinkPage/AddDrinkPage.jsx
@@ -3,14 +3,14 @@ import { Container, Title } from './AddDrinkPage.styled';
 import { AddDrinkForm } from '../../components/AddDrinkForm/AddDrinkForm';
 import { PopularDrinks } from '../../components/PopularDrinks/PopularDrinks';
 import PageTitle from '../../components/PageTitle/PageTitle';
-// імпортнути компонент фолловАс
+import FollowUs from '../../components/FollowUs/FollowUs';
 
 const AddDrinkPage = () => {
   return (
     <Container>
       <PageTitle title="test page title" />
       <AddDrinkForm />
-      {/* компонент фолловАс */}
+      <FollowUs />
       <PopularDrinks />
     </Container>
   );
