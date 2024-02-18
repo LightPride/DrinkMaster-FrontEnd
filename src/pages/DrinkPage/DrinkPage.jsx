265ea789caeb8e24346624bb9e56f766e1e6a35d
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-18 23:42:33 +0200

    1

--- src/pages/DrinkPage/DrinkPage.jsx
+++ src/pages/DrinkPage/DrinkPage.jsx
@@ -1,4 +1,4 @@
-import { Container, DrinkPageWrapper } from './DrinkPage.styled';
+import { DrinkPageWrapper } from './DrinkPage.styled';
 import DrinkPageHero from '../../components/DrinkPageHero/DrinkPageHero';
 import DrinkIngredientsList from '../../components/DrinkIngredientList/DrinkIngredientsList';
 import RecipePreparation from '../../components/RecipePreparation/RecipePreparation';
@@ -9,18 +9,16 @@ import recipes from '../../helpers/data/recipes.json';
 const DrinkPage = () => {
   return (
     <DrinkPageWrapper>
-      <Container>
-        <DrinkPageHero
-          id={recipes._id}
-          name={recipes[0].drink}
-          glass={recipes[0].glass}
-          alcoholic={recipes[0].alcoholic}
-          description={recipes[0].description}
-          imgPath={recipes[0].drinkThumb}
-        />
-        <DrinkIngredientsList ingredients={recipes[0].ingredients} />
-        <RecipePreparation instructions={recipes[0].instructions} />
-      </Container>
+      <DrinkPageHero
+        id={recipes._id}
+        name={recipes[0].drink}
+        glass={recipes[0].glass}
+        alcoholic={recipes[0].alcoholic}
+        description={recipes[0].description}
+        imgPath={recipes[0].drinkThumb}
+      />
+      <DrinkIngredientsList ingredients={recipes[0].ingredients} />
+      <RecipePreparation instructions={recipes[0].instructions} />
     </DrinkPageWrapper>
   );
 };
