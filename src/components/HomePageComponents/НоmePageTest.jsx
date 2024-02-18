bd0ba9082c6a4412a5e1527a71763b53a7b71745
Zoja7 <yaozonka@gmail.com>
2024-02-18 19:31:28 +0200

    test_div_styles

--- src/components/HomePageComponents/НоmePageTest.jsx
+++ src/components/HomePageComponents/НоmePageTest.jsx
@@ -1,7 +1,6 @@
+import { Button } from '../Button/Button';
 
-import  {Button} from '../Button/Button';
-
-import { Container } from '../../components/Layout/Container/Container';
+// import { Container } from '../../components/Layout/Container/Container';
 
 
 const НomePageTest = () => {
@@ -9,23 +8,25 @@ const НomePageTest = () => {
     console.log('Button clicked');
   };
   return (
-    <Container>
-      <Button onClick={handleClick} addRecipe>
-        Add Recipe
-      </Button>
-      <Button  onClick={handleClick} logOut>
-        Log Out
-      </Button>
-      <Button onClick={handleClick} addToFavorite>
-        Add to Favorites
-      </Button>
-      <Button onClick={handleClick} seeRecipe>
-        See Recipe
-      </Button>
-      <Button onClick={handleClick} subscribe>
-        Subscribe
-      </Button>
-    </Container>
+    // <Container>
+      <div style={{ display: 'flex', alignItems: 'center',justifyContent: 'center', flexDirection: 'column', gap: '8px', padding: '120px' }}>
+        <Button onClick={handleClick} addRecipe>
+          Add Recipe
+        </Button>
+        <Button onClick={handleClick} logOut>
+          Log Out
+        </Button>
+        <Button onClick={handleClick} addToFavorite>
+          Add to Favorites
+        </Button>
+        <Button onClick={handleClick} seeRecipe>
+          See Recipe
+        </Button>
+        <Button onClick={handleClick} subscribe>
+          Subscribe
+        </Button>
+      </div>
+    // </Container>
   );
 };
 
