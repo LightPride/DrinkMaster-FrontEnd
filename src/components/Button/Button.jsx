2b880dbbb95d86bf63580dba037011412ed7b7b7
Zoja7 <yaozonka@gmail.com>
2024-02-18 16:41:19 +0200

    commit_StyledButtons

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,67 @@
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { StyledButton } from './StyledButton.styled';
+
+export const Button = ({
+  children,
+  className,
+  addRecipe,
+  logOut,
+  addToFavorite,
+  seeRecipe,
+  subscribe,
+  disabled,
+  active,
+  onClick,
+  ...otherProps
+}) => {
+  const buttonClasses = clsx(StyledButton, className,{
+    addRecipe,
+    logOut,
+    addToFavorite,
+    seeRecipe,
+    subscribe,
+  });
+  const onClickAction = (e) => {
+    if (disabled) {
+      e.preventDefault();
+    } else {
+      return onClick(e);
+    }
+  };
+
+  const ButtonTag = otherProps.href ? 'a' : StyledButton;
+
+  return (
+    <ButtonTag
+      className={buttonClasses}
+      onClick={onClickAction}
+      active={active}
+      disabled={disabled}
+      {...otherProps}
+    >
+      {children}
+    </ButtonTag>
+  );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  active: PropTypes.bool,
+  onClick: PropTypes.func,
+  addToFavorite: PropTypes.bool,
+  seeRecipe:PropTypes.bool,
+  subscribe:PropTypes.bool,
+};
+
+Button.defaultProps = {
+  className: '',
+  disabled: false,
+  active: false,
+  addToFavorite: false,
+  seeRecipe: false,
+  subscribe: false,
+  onClick: () => {},
+};
