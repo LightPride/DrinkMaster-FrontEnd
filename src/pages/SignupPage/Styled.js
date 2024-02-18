6c5e13d90859f0469cefa67a5639453a4b193129
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2024-02-17 20:43:04 -0500

    updated

--- src/pages/SignupPage/Styled.js
+++ src/pages/SignupPage/Styled.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 import imgDesktop from '../../images/welcomeDesk@1x.jpg';
+<<<<<<< Updated upstream
 import blurDobleSercle from '../../images/BackgroundImages/blurDobleSircle.png';
 import blurSmallSercle from '../../images/BackgroundImages/blurSmallSircle.png';
 
 export const StyledSigninPage = styled.div`
   padding-top: 220px;
   background-image: url(${blurDobleSercle}), url(${blurSmallSercle}),
+=======
+import blurDobleDesctop from '../../images/BackgroundImages/blurDobleDesctop.png';
+import blurSmallDesktop from '../../images/BackgroundImages/blurSmallDecktop.png';
+
+export const StyledSigninPage = styled.div`
+  padding-top: 220px;
+  background-image: url(${blurDobleDesctop}), url(${blurSmallDesktop}),
+>>>>>>> Stashed changes
     url(${imgDesktop});
   background-repeat: no-repeat, no-repeat, no-repeat;
   background-position:
