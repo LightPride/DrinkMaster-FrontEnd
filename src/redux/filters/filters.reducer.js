bb34310619ee048c580e3c8292823976a8123aeb
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-21 00:56:12 +0200

    filters-redux 1st version

--- src/redux/filters/filters.reducer.js
+++ src/redux/filters/filters.reducer.js
@@ -1,6 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  getCategories,
+  getIngredients,
+  getGlasses,
+} from '..//filters/filters.operations';
 
 const initialState = {
+  searchQuery: '',
+  categories: [],
+  ingredients: [],
+  glasses: [],
   isLoading: false,
   error: null,
 };
@@ -8,9 +17,58 @@ const initialState = {
 const filtersSlice = createSlice({
   name: 'filters',
   initialState,
-  reducers: {},
-});
+  // reducers: {},
+  extraReducers: (builder) =>
+    builder
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.categories = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getIngredients.fulfilled, (state, action) => {
+        state.ingredients = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getGlasses.fulfilled, (state, action) => {
+        state.glasses = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addMatcher(
+        isAnyOf(
+          getCategories.pending,
+          getIngredients.pending,
+          getGlasses.pending
+        ),
+        (state) => {
+          state.isLoading = true;
+        }
+      )
 
-// const {} = filtersSlice.actions;
+      .addMatcher(
+        isAnyOf(
+          getCategories.rejected,
+          getIngredients.rejected,
+          getGlasses.rejected
+        ),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+        }
+      ),
+
+  reducers: {
+    setSearchQuery: {
+      reducer: (state, action) => {
+        state.searchQuery = action.payload;
+      },
+
+      prepare: (searchQuery) => {
+        return { payload: searchQuery };
+      },
+    },
+  },
+});
 
 export const filtersReducer = filtersSlice.reducer;
