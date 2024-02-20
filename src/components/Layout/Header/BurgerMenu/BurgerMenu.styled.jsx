bcd71c08b534131f4045143b35e2809cb812e4cd
Serhii Filatov <filser.spirit@gmail.com>
2024-02-20 21:19:46 +0200

    Adds a background to the burgerMenu

--- src/components/Layout/Header/BurgerMenu/BurgerMenu.styled.jsx
+++ src/components/Layout/Header/BurgerMenu/BurgerMenu.styled.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import bgBurgerMenu from '../../../../images/BackgroundImages/bgBurgerMenu.webp';
+
 export const BurgerContainer = styled.div`
   position: relative;
   z-index: 100;
@@ -24,6 +26,10 @@ export const MenuItems = styled.ul`
     right: 0;
     bottom: 0;
     background-color: var(--black-color);
+    background-image: url(${bgBurgerMenu});
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-size: cover;
     z-index: 100;
     @media screen and (min-width: 768px) {
       top: 87px;
