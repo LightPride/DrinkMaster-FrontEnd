bb060b1d2e2a54b5f2f8caccaa51766a1d168bc2
LightPride <32414745+LightPride@users.noreply.github.com>
2024-02-20 18:42:34 +0200

    Update HomePageHero.styled.jsx

--- src/components/HomePageHero/HomePageHero.styled.jsx
+++ src/components/HomePageHero/HomePageHero.styled.jsx
@@ -1,54 +1,98 @@
 import styled from 'styled-components';
 
 export const HomePageHeroWrapper = styled.div`
-   padding-top: 80px;
+  @media screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   @media screen and (min-width: 768px) {
-    padding-top: 120px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   @media screen and (min-width: 1440px) {
-    padding-top: 185px;
     display: flex;
     flex-direction: row;
+    align-items: normal;
     justify-content: space-between;
   }
 `;
 
 export const HomePageTextWrapper = styled.div`
+  @media screen and (max-width: 767px) {
+    padding-top: 80px;
+  }
+  @media screen and (min-width: 768px) {
+    padding-top: 120px;
+  }
   @media screen and (min-width: 1440px) {
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    flex-direction: column;
-    gap: 40px;
+    padding-top: 185px;
     width: 715px;
-    height: 458px;
   }
 `;
 
-export const HomePageImageWrapper = styled.div``;
+export const HomePageImageWrapper = styled.div`
+  @media screen and (max-width: 767px) {
+    margin-top: 47px;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 54px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-top: 0px;
+    padding-top: 128px;
+  }
+`;
 
 export const HomePageTitle = styled.div`
   font-size: 32px;
   font-weight: 600;
+  line-height: 38px;
+  letter-spacing: 0%;
   color: var(--white-color);
-
   @media screen and (min-width: 768px) {
     font-size: 56px;
+    font-weight: 600;
+    line-height: 60px;
+    letter-spacing: 0%;
   }
 
   @media screen and (min-width: 1440px) {
     font-size: 64px;
+    font-weight: 600;
+    line-height: 68px;
+    letter-spacing: 0%;
   }
 `;
 export const HomePageSubtitle = styled.p`
-  /* padding-right: 124px; */
-  padding-bottom: 128px;
-  
+  @media screen and (max-width: 767px) {
+    margin-top: 16px;
+    margin-bottom: 32px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0%;
+  }
+  @media screen and (min-width: 768px) {
+    margin-top: 28px;
+    margin-bottom: 48px;
+    max-width: 600px;
+
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0%;
+  }
+  @media screen and (min-width: 1440px) {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0%;
+    margin-top: 28px;
+    margin-bottom: 40px;
+  }
   color: #f3f3f3;
-  font-size: 600;
-  line-height:1.6;
-  max-width: 500px;
-  max-height:76px
 `;
 
 export const HomePageImage = styled.img`
