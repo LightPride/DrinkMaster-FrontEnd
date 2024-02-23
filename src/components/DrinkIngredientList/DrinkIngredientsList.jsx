aa358b6fc2cc63959652d35a4ea4ac5b6738729f
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-24 00:26:13 +0200

    add 3 size ingredientThumb

--- src/components/DrinkIngredientList/DrinkIngredientsList.jsx
+++ src/components/DrinkIngredientList/DrinkIngredientsList.jsx
@@ -6,7 +6,7 @@ import {
 import { nanoid } from 'nanoid';
 
 const DrinkIngredientsList = ({ ingredients }) => {
-  console.log(ingredients);
+  // console.log(ingredients);
   // console.log(ingredients[0].ingredient.ingredientThumb);
   return (
     <>
@@ -14,20 +14,34 @@ const DrinkIngredientsList = ({ ingredients }) => {
         <>
           <IngredientsTitle>Ingredients</IngredientsTitle>
           <IngredientsList>
-            {ingredients.map((ingredient) => (
-              <li key={nanoid()}>
-                {/* // <li key={ingredient.ingredient && ingredient.ingredient._id}> */}
-                <DrinkIngredientItem
-                  title={ingredient.title}
-                  measure={ingredient.measure}
-                  quantity={ingredient.quantity}
-                  image={
-                    ingredient.ingredient &&
-                    ingredient.ingredient.ingredientThumb
-                  }
-                />
-              </li>
-            ))}
+            {ingredients.map((ingredient) => {
+              const images = {
+                ingredientThumb: '',
+                ['thumb-medium']: '',
+                ['thumb-small']: '',
+              };
+              // console.log(images);
+              if (
+                ingredient.ingredient &&
+                ingredient.ingredient.ingredientThumb
+              ) {
+                images.ingredientThumb = ingredient.ingredient.ingredientThumb;
+                images['thumb-medium'] = ingredient.ingredient['thumb-medium'];
+                images['thumb-small'] = ingredient.ingredient['thumb-small'];
+              }
+              // console.log(images);
+              return (
+                <li key={nanoid()}>
+                  {/* // <li key={ingredient.ingredient && ingredient.ingredient._id}> */}
+                  <DrinkIngredientItem
+                    title={ingredient.title}
+                    measure={ingredient.measure}
+                    quantity={ingredient.quantity}
+                    images={images}
+                  />
+                </li>
+              );
+            })}
           </IngredientsList>
         </>
       )}
