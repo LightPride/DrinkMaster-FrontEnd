9e1b3188b7b7bc9523d67f2df532693f5faf511f
LightPride <vlad.grinevich1@gmail.com>
2024-02-24 22:04:35 +0200

    added mainpage drinks responsive request

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,11 +7,12 @@ import {
   selectIsLoading,
   selectErrorDrinks,
 } from '../../redux/drinks/drinks.selectors';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getMainPageDrinks } from '../../redux/drinks/drinks.operations';
 import HomePageList from '../../components/HomePageList/HomePageList';
 
 const HomePage = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const dispatch = useDispatch();
   const drinks = useSelector(selectMainPageDrinks);
   const isLoadingFromRedux = useSelector(selectIsLoading);
@@ -20,9 +21,31 @@ const HomePage = () => {
   const showCocktails =
     !error && !isLoadingFromRedux && drinks && drinks.length > 0;
 
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  const setQuantity = (windowWidth) => {
+    let quantity = 0;
+    if (windowWidth < 768) {
+      quantity = 1;
+    } else if (windowWidth >= 768 && windowWidth < 1440) {
+      quantity = 2;
+    } else if (windowWidth >= 1440) {
+      quantity = 3;
+    }
+    return quantity;
+  };
+
   useEffect(() => {
-    dispatch(getMainPageDrinks());
-  }, [dispatch]);
+    window.addEventListener('resize', handleWindowResize);
+    const quantity = setQuantity(windowWidth);
+
+    dispatch(getMainPageDrinks(quantity));
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, [dispatch, windowWidth]);
 
   return (
     <Container>
