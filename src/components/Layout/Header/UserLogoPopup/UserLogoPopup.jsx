50cec91aa10dbe5681bf78f6a0f87130516a1b79
Serhii Filatov <filser.spirit@gmail.com>
2024-02-24 16:43:54 +0200

    adds logOutModal window

--- src/components/Layout/Header/UserLogoPopup/UserLogoPopup.jsx
+++ src/components/Layout/Header/UserLogoPopup/UserLogoPopup.jsx
@@ -3,40 +3,83 @@ import {
   StyledTypography,
   StyledPopupWindow,
 } from './UserLogoPopup.styled';
-
-import EditIcon from '@mui/icons-material/Edit';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logOutThunk } from '../../../../redux/auth/auth.operations';
+import { useState, useRef, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { IconButton } from '@mui/material';
-import { logOutThunk } from '../../../../redux/auth/auth.operations';
-import { useDispatch } from 'react-redux';
+import EditIcon from '@mui/icons-material/Edit';
+
+import { LogOutModal } from '../LogOutModal/LogOutModal';
 
 export const UserLogoPopup = ({ isPopupOpen, setIsPopupOpen }) => {
   const dispatch = useDispatch();
-  console.log('Сработала кнопка!', isPopupOpen);
-  console.log(setIsPopupOpen);
+  const navigate = useNavigate();
+  const modalRef = useRef(null);
+
+  const [isLogOutOpen, SetLogOutOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setIsPopupOpen(false);
+      }
+    };
+    if (isPopupOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    // UNDONE ===================================================================
+    if (isLogOutOpen) {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isPopupOpen, setIsPopupOpen, isLogOutOpen]);
+
+  const handleLogout = (values) => {
+    try {
+      dispatch(logOutThunk(values));
+      navigate('/welcome');
+    } catch (error) {
+      navigate('/welcome');
+    }
+  };
+
+  const handleLogOutClick = () => {
+    SetLogOutOpen((prev) => !prev);
+  };
 
   return (
-    <StyledPopupWindow>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: '38px',
-        }}
-      >
-        <StyledTypography>Edit profile</StyledTypography>
-        <IconButton>
-          <EditIcon sx={{ width: '14px', height: '14px', color: '#F3F3F3' }} />
-        </IconButton>
-      </Box>
-      <LogOutBtn
-        onClick={() => {
-          dispatch(logOutThunk());
-        }}
-      >
-        Log out
-      </LogOutBtn>
-    </StyledPopupWindow>
+    <>
+      <StyledPopupWindow>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '38px',
+          }}
+        >
+          <StyledTypography>Edit profile</StyledTypography>
+          <IconButton>
+            <EditIcon
+              sx={{ width: '14px', height: '14px', color: '#F3F3F3' }}
+            />
+          </IconButton>
+        </Box>
+        <LogOutBtn onClick={handleLogOutClick}>Log out</LogOutBtn>
+      </StyledPopupWindow>
+      {isLogOutOpen && (
+        <LogOutModal
+          isOpen={isLogOutOpen}
+          onLogOut={handleLogout}
+          onClose={handleLogOutClick}
+        ></LogOutModal>
+      )}
+    </>
   );
 };
