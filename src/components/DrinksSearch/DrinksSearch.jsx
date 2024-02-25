6458458e02b81fa43bbd153d83ccbbed4402cf35
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2024-02-25 16:56:13 +0200

    changed the logic of storing the search query and adding parameters to the URL

--- src/components/DrinksSearch/DrinksSearch.jsx
+++ src/components/DrinksSearch/DrinksSearch.jsx
@@ -11,77 +11,74 @@ import {
 import { getSearchedDrink } from '../../redux/drinks/drinks.operations';
 import { Filter, Wrapper } from './DrinksSearch.styled';
 import Selection from './Select';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const DrinksSearch = () => {
   const dispatch = useDispatch();
   const categories = useSelector(selectCategories);
   const ingredients = useSelector(selectIngredients);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const [name, setName] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [selectedIngredient, setSelectedIngredient] = useState('');
+  const queryParams = new URLSearchParams(location.search);
+  const [searchQuery, setSearchQuery] = useState(queryParams.get('name') || '');
+  const [selectedCategory, setSelectedCategory] = useState(
+    queryParams.get('category') || ''
+  );
+  const [selectedIngredient, setSelectedIngredient] = useState(
+    queryParams.get('ingredient') || ''
+  );
 
   useEffect(() => {
     dispatch(getCategories());
     dispatch(getIngredients());
-    const savedSearchQuery = localStorage.getItem('searchQuery');
-    if (savedSearchQuery) {
-      setName(savedSearchQuery);
-      dispatch(getSearchedDrink({ name: savedSearchQuery }));
-    }
-    const savedCategory = localStorage.getItem('selectedCategory');
-    if (savedCategory) {
-      setSelectedCategory(savedCategory);
-    }
-    const savedIngredient = localStorage.getItem('selectedIngredient');
-    if (savedIngredient) {
-      setSelectedIngredient(savedIngredient);
-    }
   }, [dispatch]);
+
+  useEffect(() => {
+    // Функція для виконання пошуку та оновлення URL
+    const searchAndNavigate = async () => {
+      await dispatch(
+        getSearchedDrink({
+          name: searchQuery,
+          category: selectedCategory,
+          ingredient: selectedIngredient,
+        })
+      );
+
+      // Оновлення URL після отримання результатів пошуку
+      const params = new URLSearchParams();
+      if (searchQuery) params.append('name', searchQuery);
+      if (selectedCategory) params.append('category', selectedCategory);
+      if (selectedIngredient) params.append('ingredient', selectedIngredient);
+
+      const queryString = params.toString();
+      console.log('New URL parameters:', queryString);
+      navigate(`?${queryString}`);
+      console.log('URL updated');
+    };
+
+    searchAndNavigate();
+  }, [dispatch, searchQuery, selectedCategory, selectedIngredient, navigate]);
 
   const handleChangeName = (event) => {
     const value = event.target.value;
-    setName(value);
-    localStorage.setItem('searchQuery', value);
+    setSearchQuery(value);
   };
 
   const handleCategoryChange = (selectedOption) => {
-    if (selectedOption) {
-      const value = selectedOption.value;
-      setSelectedCategory(value);
-      localStorage.setItem('selectedCategory', value);
-    } else {
-      setSelectedCategory(''); // Очищення вибраної категорії
-      localStorage.removeItem('selectedCategory'); // Видалення ключа з локального сховища
-    }
+    const value = selectedOption ? selectedOption.value : '';
+    setSelectedCategory(value);
   };
 
   const handleIngredientChange = (selectedOption) => {
-    if (selectedOption) {
-      const value = selectedOption.value;
-      setSelectedIngredient(value);
-      localStorage.setItem('selectedIngredient', value);
-    } else {
-      setSelectedIngredient(''); // Очищення вибраного інгредієнта
-      localStorage.removeItem('selectedIngredient'); // Видалення ключа з локального сховища
-    }
+    const value = selectedOption ? selectedOption.value : '';
+    setSelectedIngredient(value);
   };
 
-  useEffect(() => {
-    dispatch(
-      getSearchedDrink({
-        name,
-        category: selectedCategory,
-        ingredient: selectedIngredient,
-      })
-    );
-    localStorage.setItem('searchQuery', name);
-  }, [dispatch, name, selectedCategory, selectedIngredient]);
-
-  // Фільтрація опцій для відображення вибору
   const filteredCategories = categories
     .map((category) => category.category)
     .filter((category) => category !== selectedCategory);
+
   const filteredIngredients = ingredients
     .map((ingredient) => ingredient.title)
     .filter((ingredient) => ingredient !== selectedIngredient);
@@ -92,7 +89,7 @@ const DrinksSearch = () => {
         id="filterInput"
         type="text"
         placeholder="Enter the text"
-        value={name}
+        value={searchQuery}
         onChange={handleChangeName}
         debounceTimeout={1000}
       />
