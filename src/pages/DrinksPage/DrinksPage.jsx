ac36f974a98f0d0f247316d086df19b883741bf0
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2024-02-26 20:02:58 +0200

    add

--- src/pages/DrinksPage/DrinksPage.jsx
+++ src/pages/DrinksPage/DrinksPage.jsx
@@ -31,9 +31,11 @@ export default function DrinksPage() {
         ) : error || drinks.length === 0 ? (
           <ErrorMessage>No cocktails were found for your request</ErrorMessage>
         ) : (
-          <Drinks drinks={drinks} />
+          <>
+            <Drinks drinks={drinks} />
+            <Paginator />
+          </>
         )}
-        <Paginator />
       </Wrapper>
     </Container>
   );
